30a0816e9794c58773848912801892f300bbfc6d
JonWong2020 <jonathannwong50@aol.com>
2022-09-21 20:04:18 -0700

    initial commit

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const mysql = require('mysql2');
+const env = process.env;
+
+const connection = mysql.createConnection({
+    database: env.DB_NAME,
+    user: env.DB_USER,
+    password: env.DB_PASSWORD,
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  
+});
+
+module.exports = connection;
