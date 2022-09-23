b5a9d8e2b036d3333ac034fcdaa69b7734e0ad5e
JonWong2020 <jonathannwong50@aol.com>
2022-09-22 22:39:04 -0700

    updated render

--- libs/db-render.js
+++ libs/db-render.js
@@ -1,16 +1,67 @@
-const dbQuery = require('./db-query');
+const {prompt} = require('inquirer');
+const {
+    getAllDepartments,
+    getAllRoles,
+    getAllEmployees,
+    addDepartment,
+    addRole
+} = require('./db-query');
 
 const viewDepartments = async () =>
-    console.table(await dbQuery.getAllDepartments());
+    console.table(await getAllDepartments());
 
 const viewEmployees = async () =>
-    console.table(await dbQuery.getAllEmployees());
+    console.table(await getAllEmployees());
 
 const viewRoles = async () =>
-    console.table(await dbQuery.getAllRoles());
+    console.table(await getAllRoles());
+
+const addDepartmentOption = async () => {
+    const {department} = await prompt({
+        name: 'department',
+        message: 'Enter your department name:'
+    });
+
+    await addDepartment(department);
+    console.log('Department added');
+    await viewDepartments();
+}
+
+const addRoleOption = async () => {
+    const departments = (await getAllDepartments()).map(department => ({
+        name: department.name,
+        value: department.id,
+    }));
+
+    const {
+        roleName,
+        roleSalary,
+        roleDepartment,
+    } = await prompt([
+        {
+            name: 'roleName',
+            message: 'Enter the name of your role:',
+        },
+        {
+            name: 'roleSalary',
+            message: 'Enter the salary for your role:',
+        },
+        {
+            type: 'list',
+            name: 'roleDepartment',
+            message: 'Enter the department for your role:',
+            choices: departments,
+        },
+    ]);
+
+    await addRole([roleName, roleSalary, roleDepartment]);
+    await viewRoles();
+};
 
 module.exports = {
     viewDepartments,
     viewEmployees,
     viewRoles,
+    addDepartmentOption,
+    addRoleOption,
 }
